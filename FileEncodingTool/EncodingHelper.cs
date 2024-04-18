844368c8d63326b711debc7ad496edba03c085f1
charles <charles76680119@gmail.com>
2024-04-18 14:35:54 +0800

    [feat] : complete base feature

---
+++ FileEncodingTool/EncodingHelper.cs
@@ -0,0 +1,57 @@
+﻿namespace EncodingChecker;
+
+public static class EncodingHelper
+{
+    /// <summary>
+    /// Detect BOM header
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns>
+    /// length of BOM header
+    /// </returns>
+    public static int DetectBOM(byte[] content)
+    {
+        if (content.Length < 4)
+            return 0;
+        var buffer = new byte[4]; // BOM are shorter than 4 bytes
+        Array.Copy(content, buffer, 4);
+        // 判断 BOM
+        if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
+            return 3;
+        if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+            return 2;
+        if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+            return 2;
+        if (buffer[0] == 0xFF && buffer[1] == 0xFE && buffer[2] == 0x00 && buffer[3] == 0x00)
+            return 4;
+        if (buffer[0] == 0x00 && buffer[1] == 0x00 && buffer[2] == 0xFE && buffer[3] == 0xFF)
+            return 2;
+        return 0;
+    }
+
+    /// <summary>
+    /// Remove BOM header
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="bomLength"></param>
+    /// <returns>byte array after remove</returns>
+    public static byte[] RemoveBOM(byte[] content, int bomLength)
+    {
+        if (content.Length < bomLength)
+            return (byte[])content.Clone();
+        var result = new byte[content.Length - bomLength];
+        Array.Copy(content, bomLength, result, 0, content.Length - bomLength);
+        return result;
+    }
+
+    /// <summary>
+    /// auto-detect BOM and remove
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns>byte array after remove</returns>
+    public static byte[] RemoveBOM(byte[] content)
+    {
+        var bomLength = DetectBOM(content);
+        return RemoveBOM(content, bomLength);
+    }
+}
---
+++ FileEncodingTool/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using System.Text;
+using EncodingChecker;
+using UtfUnknown;
+
+var rootCommand = new RootCommand("Check file encoding")
+{
+    new Option<string>(
+        new[] { "--directory", "-d" },
+        Directory.GetCurrentDirectory,
+        "target directory"),
+    new Option<string>(
+        new[] { "--target", "-t" },
+        () => "",
+        "Convert target encoding; if not specified, only detect files."),
+    new Option<string[]>(
+        new[] { "--patterns", "-p" },
+        "To detect files with a search patterns."
+    ) { IsRequired = true, AllowMultipleArgumentsPerToken = true },
+    new Option<bool>(new[] { "--sub-dir" },
+        () => true,
+        "Include subdirectories, default is true.")
+};
+
+rootCommand.Handler = CommandHandler.Create(
+    async (string directory, string target, string[] patterns, bool subDir) =>
+    {
+        if (patterns.Length == 0)
+        {
+            await Console.Error.WriteLineAsync("pattern is not set");
+            return 1;
+        }
+
+        Encoding? targetEncoding = null;
+        if (!string.IsNullOrEmpty(target))
+            try
+            {
+                targetEncoding = Encoding.GetEncoding(target);
+            }
+            catch (Exception)
+            {
+                await Console.Error.WriteLineAsync("target encoding is not support");
+                return 1;
+            }
+
+        DirectoryInfo dirInfo = new(directory);
+        foreach (var pattern in patterns)
+        {
+            var fileInfos = dirInfo.GetFiles(pattern,
+                subDir ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            foreach (var fileInfo in fileInfos)
+            {
+                var detect = CharsetDetector.DetectFromFile(fileInfo.FullName);
+                var detectDetail = detect.Detected;
+
+                Encoding? srcEncoding;
+                // if known encoding, use detected encoding
+                if (detectDetail.EncodingName != "windows-1252" && detectDetail.Confidence >= 0.7)
+                {
+                    Console.WriteLine($"{fileInfo.FullName} Detect : {detectDetail.EncodingName}");
+                    srcEncoding = detectDetail.Encoding;
+                }
+                else
+                {
+                    await Console.Error.WriteLineAsync($"{fileInfo.FullName} Detect fail");
+                    continue;
+                }
+
+                if (targetEncoding == null) continue;
+                var content = await File.ReadAllBytesAsync(fileInfo.FullName);
+
+
+                content = Encoding.Convert(srcEncoding, targetEncoding, content);
+                if (Equals(targetEncoding, Encoding.UTF8) || Equals(targetEncoding, Encoding.Unicode) ||
+                    Equals(targetEncoding, Encoding.UTF32))
+                    content = EncodingHelper.RemoveBOM(content);
+                await File.WriteAllBytesAsync(fileInfo.FullName, content);
+                Console.WriteLine($"Converted {fileInfo.FullName} to {targetEncoding.BodyName}");
+            }
+        }
+
+        return 0;
+    });
+
+var errorCode = await rootCommand.InvokeAsync(args);
+Console.WriteLine($"Complete with error code {errorCode}");
+return errorCode;
